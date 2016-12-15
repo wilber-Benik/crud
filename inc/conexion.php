1dc5d1a0a2f9273484d026f9eb07e40dde490fc7
wilber-Benik <wilberforever@gmail.com>
2016-12-15 18:01:05 -0500

    obtimizando 1

--- inc/conexion.php
+++ inc/conexion.php
@@ -1,5 +1,5 @@
 <?php 
-	 $conexion=new mysqli("localhost","u504756857_wil","wilber123",'u504756857_wil');
+	 $conexion=new mysqli("mysql.hostinger.es","u504756857_wil","wilber123",'u504756857_wil');
 	 if ($conexion->connect_errno){
 	 	echo "Error al conectar ala BB DD:".
 	 		$conexion->connect_errno.",".
